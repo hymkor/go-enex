ec1f92326ceceaf38cf13d981b806d412d9ab802
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-04-07 20:39:46 +0900

    Add -markdown option

--- cmd/enexToHtml/main.go
+++ cmd/enexToHtml/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/mattn/godown"
 	"github.com/zetamatta/go-enex"
 )
+
+var optionMarkdown = flag.Bool("markdown", false, "output markdown")
 
 func mains(args []string) error {
 	var data []byte
 	var err error
 	var output io.Writer
-	prefix := ""
+	baseName := ""
 
 	if len(args) <= 0 {
 		data, err = io.ReadAll(os.Stdin)
@@ -27,21 +32,29 @@ func mains(args []string) error {
 			return err
 		}
 		ext := filepath.Ext(args[0])
-		prefix = args[0][:len(args[0])-len(ext)]
-		fd, err := os.Create(prefix + ".html")
+		baseName = args[0][:len(args[0])-len(ext)]
+		outputSuffix := ".html"
+		if *optionMarkdown {
+			outputSuffix = ".md"
+		}
+		fd, err := os.Create(baseName + outputSuffix)
 		if err != nil {
 			return err
 		}
 		defer fd.Close()
 		output = fd
-		prefix = prefix + "-"
+		baseName = baseName + "-"
 	}
 	en, err := enex.Parse(data)
 	if err != nil {
 		return err
 	}
-	html, attachment := en.Html(prefix)
-	io.WriteString(output, html)
+	html, attachment := en.Html(baseName)
+	if *optionMarkdown {
+		godown.Convert(output, strings.NewReader(html), &godown.Option{})
+	} else {
+		io.WriteString(output, html)
+	}
 	for fname, data := range attachment {
 		fmt.Fprintf(os.Stderr, "Create File: %s (%d bytes)\n", fname, len(data))
 		os.WriteFile(fname, data, 0666)
@@ -50,7 +63,8 @@ func mains(args []string) error {
 }
 
 func main() {
-	if err := mains(os.Args[1:]); err != nil {
+	flag.Parse()
+	if err := mains(flag.Args()); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zetamatta/go-enex
 
 go 1.16
+
+require github.com/mattn/godown v0.0.1 // indirect
