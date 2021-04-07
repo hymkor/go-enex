2f8a1df889f883443adb6944aaa7c6f5b0aed5db
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-04-07 20:02:33 +0900

    Get *.enex path from argv[0]

--- cmd/enexToHtml/main.go
+++ cmd/enexToHtml/main.go
@@ -4,21 +4,44 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/zetamatta/go-enex"
 )
 
-func mains() error {
-	data, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		return err
+func mains(args []string) error {
+	var data []byte
+	var err error
+	var output io.Writer
+	prefix := ""
+
+	if len(args) <= 0 {
+		data, err = io.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+		output = os.Stdout
+	} else {
+		data, err = os.ReadFile(args[0])
+		if err != nil {
+			return err
+		}
+		ext := filepath.Ext(args[0])
+		prefix = args[0][:len(args[0])-len(ext)]
+		fd, err := os.Create(prefix + ".html")
+		if err != nil {
+			return err
+		}
+		defer fd.Close()
+		output = fd
+		prefix = prefix + "-"
 	}
 	en, err := enex.Parse(data)
 	if err != nil {
 		return err
 	}
-	html, attachment := en.Html("")
-	io.WriteString(os.Stdout, html)
+	html, attachment := en.Html(prefix)
+	io.WriteString(output, html)
 	for fname, data := range attachment {
 		fmt.Fprintf(os.Stderr, "Create File: %s (%d bytes)\n", fname, len(data))
 		os.WriteFile(fname, data, 0666)
@@ -27,7 +50,7 @@ func mains() error {
 }
 
 func main() {
-	if err := mains(); err != nil {
+	if err := mains(os.Args[1:]); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
