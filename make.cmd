e4279d71357e18206d01b7c39f6ce2f727a54bfa
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-04-07 21:07:52 +0900

    Add mainteinance batch (make.cmd)

---
+++ make.cmd
@@ -0,0 +1,23 @@
+call :"%~1"
+exit /b
+
+:""
+:"all"
+    go fmt || exit /b
+    go build || exit /b
+    pushd cmd\enexToHtml || exit /b
+    go fmt && go build
+    popd
+    exit /b
+
+:"clean"
+    del *.png *.html *.md
+    exit /b
+
+:"html"
+    for %%I in (*.enex) do cmd\enexToHtml\enexToHtml.exe "%%~I"
+    exit /b
+
+:"md"
+    for %%I in (*.enex) do cmd\enexToHtml\enexToHtml.exe -markdown "%%~I"
+    exit /b
