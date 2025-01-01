4117ae0031b9d2e35ddc618f14341b4aaacc0851
HAYAMA_Kaoru <iyahaya@nifty.com>
2025-01-01 20:46:11 +0900

    refactor: unenex.go: split mains function

--- cmd/unenex/main.go
+++ cmd/unenex/main.go
@@ -22,6 +22,119 @@ var (
 	optionStyleInline = flag.String("st", "", "Specify stylesheet text directly as a string.")
 )
 
+func extractAttachment(attachment map[string]*enex.Resource) error {
+	for fname, data := range attachment {
+		dir := filepath.Dir(fname)
+		if stat, err := os.Stat(dir); os.IsNotExist(err) {
+			fmt.Fprintln(os.Stderr, "Create Dir:", dir)
+			if err := os.Mkdir(dir, 0755); err != nil {
+				return err
+			}
+		} else if err != nil {
+			return err
+		} else if !stat.IsDir() {
+			return fmt.Errorf("Can not mkdir %s because the file same name already exists", dir)
+		}
+		fmt.Fprint(os.Stderr, "Create File: ", fname)
+		fd, err := os.Create(fname)
+		if err != nil {
+			fmt.Fprintln(os.Stderr)
+			return fmt.Errorf("os.Create: %w", err)
+		}
+		n, err := data.WriteTo(fd)
+		if err != nil {
+			fmt.Fprintln(os.Stderr)
+			return fmt.Errorf(".WriteTo: %w", err)
+		}
+		if err := fd.Close(); err != nil {
+			fmt.Fprintln(os.Stderr)
+			return fmt.Errorf(".Close: %w", err)
+		}
+		fmt.Fprintf(os.Stderr, " (%d bytes)\n", n)
+	}
+	return nil
+}
+
+func enexToMarkdown(name string, source []byte, styleSheet string, verbose io.Writer) error {
+	exports, err := enex.ParseMulti(source, verbose)
+	if err != nil {
+		return err
+	}
+	index, err := os.Create("README.md")
+	if err != nil {
+		return err
+	}
+	defer index.Close()
+
+	for _, note := range exports {
+		safeName := enex.ToSafe.Replace(note.Title)
+
+		fmt.Fprintf(index, "* [%s](%s)\n",
+			note.Title,
+			url.PathEscape(safeName+".md"),
+		)
+
+		html, imgSrc := note.HtmlAndDir()
+
+		var markdown strings.Builder
+		godown.Convert(&markdown, strings.NewReader(html), nil)
+		fd, err := os.Create(safeName + ".md")
+		if err != nil {
+			return err
+		}
+		enex.ShrinkMarkdown(strings.NewReader(markdown.String()), fd)
+		fd.Close()
+		fmt.Println("Create File:", safeName+".md")
+
+		if err := extractAttachment(imgSrc.Images); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func enexToHtml(name string, source []byte, styleSheet string, verbose io.Writer) error {
+	exports, err := enex.ParseMulti(source, verbose)
+	if err != nil {
+		return err
+	}
+	index, err := os.Create("index.html")
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(index, `<html><head>`)
+	fmt.Fprintln(index, `<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">`)
+	fmt.Fprintln(index, `</head><body><ul>`)
+	defer func() {
+		fmt.Fprintln(index, "</ul></body></html>")
+		index.Close()
+	}()
+
+	for _, note := range exports {
+		note.ExHeader = styleSheet
+
+		safeName := enex.ToSafe.Replace(note.Title)
+
+		fmt.Fprintf(index, "<li><a href=\"%s\">%s</a></li>\n",
+			url.PathEscape(safeName+".html"),
+			note.Title,
+		)
+		html, imgSrc := note.HtmlAndDir()
+		fd, err := os.Create(safeName + ".html")
+		if err != nil {
+			return err
+		}
+		io.WriteString(fd, html)
+		fd.Close()
+		fmt.Println("Create File:", safeName+".html")
+
+		if err := extractAttachment(imgSrc.Images); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func mains(args []string) error {
 	var data []byte
 	var err error
@@ -50,15 +163,17 @@ func mains(args []string) error {
 		styleSheet = fmt.Sprintf("<style>\n%s\n</style>\n", *optionStyleInline)
 	}
 
-	var exports []*enex.Export
+	outfunc := enexToHtml
+	if *optionMarkdown {
+		outfunc = enexToMarkdown
+	}
 
 	if len(args) <= 0 {
-		data, err = io.ReadAll(os.Stdin)
+		source, err := io.ReadAll(os.Stdin)
 		if err != nil {
 			return err
 		}
-		exports, err = enex.ParseMulti(data, verbose)
-		if err != nil {
+		if err := outfunc("enex", source, "", verbose); err != nil {
 			return err
 		}
 	} else {
@@ -75,100 +190,9 @@ func mains(args []string) error {
 			if err != nil {
 				return err
 			}
-			_exports, err := enex.ParseMulti(data, verbose)
-			if err != nil {
+			if err := outfunc(arg, data, styleSheet, verbose); err != nil {
 				return err
 			}
-			exports = append(exports, _exports...)
-		}
-	}
-	for _, x := range exports {
-		x.ExHeader = styleSheet
-	}
-	var index *os.File
-	if *optionMarkdown {
-		var err error
-		index, err = os.Create("README.md")
-		if err != nil {
-			return err
-		}
-		defer index.Close()
-	} else {
-		var err error
-		index, err = os.Create("index.html")
-		if err != nil {
-			return err
-		}
-		fmt.Fprintln(index, `<html><head>`)
-		fmt.Fprintln(index, `<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">`)
-		fmt.Fprintln(index, `</head><body><ul>`)
-		defer func() {
-			fmt.Fprintln(index, "</ul></body></html>")
-			index.Close()
-		}()
-	}
-	for _, note := range exports {
-		safeName := enex.ToSafe.Replace(note.Title)
-
-		if *optionMarkdown {
-			fmt.Fprintf(index, "* [%s](%s)\n",
-				note.Title,
-				url.PathEscape(safeName+".md"),
-			)
-		} else {
-			fmt.Fprintf(index, "<li><a href=\"%s\">%s</a></li>\n",
-				url.PathEscape(safeName+".html"),
-				note.Title,
-			)
-		}
-		html, imgSrc := note.HtmlAndDir()
-		if *optionMarkdown {
-			var markdown strings.Builder
-			godown.Convert(&markdown, strings.NewReader(html), nil)
-			fd, err := os.Create(safeName + ".md")
-			if err != nil {
-				return err
-			}
-			enex.ShrinkMarkdown(strings.NewReader(markdown.String()), fd)
-			fd.Close()
-			fmt.Println("Create File:", safeName+".md")
-		} else {
-			fd, err := os.Create(safeName + ".html")
-			if err != nil {
-				return err
-			}
-			io.WriteString(fd, html)
-			fd.Close()
-			fmt.Println("Create File:", safeName+".html")
-		}
-		for fname, data := range imgSrc.Images {
-			dir := filepath.Dir(fname)
-			if stat, err := os.Stat(dir); os.IsNotExist(err) {
-				fmt.Fprintln(os.Stderr, "Create Dir:", dir)
-				if err := os.Mkdir(dir, 0755); err != nil {
-					return err
-				}
-			} else if err != nil {
-				return err
-			} else if !stat.IsDir() {
-				return fmt.Errorf("Can not mkdir %s because the file same name already exists", dir)
-			}
-			fmt.Fprint(os.Stderr, "Create File: ", fname)
-			fd, err := os.Create(fname)
-			if err != nil {
-				fmt.Fprintln(os.Stderr)
-				return fmt.Errorf("os.Create: %w", err)
-			}
-			n, err := data.WriteTo(fd)
-			if err != nil {
-				fmt.Fprintln(os.Stderr)
-				return fmt.Errorf(".WriteTo: %w", err)
-			}
-			if err := fd.Close(); err != nil {
-				fmt.Fprintln(os.Stderr)
-				return fmt.Errorf(".Close: %w", err)
-			}
-			fmt.Fprintf(os.Stderr, " (%d bytes)\n", n)
 		}
 	}
 	return nil
