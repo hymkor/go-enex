c601ec11d630af63f97ad7be914884f7a9231363
HAYAMA_Kaoru <iyahaya@nifty.com>
2025-01-07 00:10:31 +0900

    Insert `<!DOCTYPE html>` at the top of HTML

--- unenex-html.go
+++ unenex-html.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 )
 
-const indexHtmlHeader = `<html><head>
+const indexHtmlHeader = `<!DOCTYPE html><html><head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head><body>`
 
